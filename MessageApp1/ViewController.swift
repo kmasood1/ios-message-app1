112fed282cbf04e198cf66f6a5bea4b05cb4b95b
Kashif Masood <kashifisn@me.com>
2016-04-26 09:49:50 -0600

    Initial Commit

---
+++ MessageApp1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MessageApp1
+//
+//  Created by A805976 on 4/26/16.
+//  Copyright © 2016 pulsartech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
